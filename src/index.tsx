33c34980ef46eb2baec1a8862556a2948777fba4
maxon-h <maks.hayduk@gmail.com>
2019-05-27 18:31:46 +0300

    Made todoList

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { store, TodoApp } from './components/Todo';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const render = () => {
+  ReactDOM.render(
+    <TodoApp {...store.getState()}/>,
+    document.getElementById('root')
+    );
+}
+store.subscribe(render);
+render();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
